23d583f5d721f4c81da4a059d0698296bb2b1ed8
Jhake <Jhake@KOYKOY>
2025-03-31 20:47:53 +0800

    Adding Fibonacci

---
+++ Recursive Fibonacci Sequence/Form1.cs
@@ -0,0 +1,23 @@
+namespace Recursive_Fibonacci_Sequence
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public int RecursiveFibonacci(int n)
+        {
+            if (n <= 1) return n;
+            return RecursiveFibonacci(n - 1) + RecursiveFibonacci(n - 2) ;
+        }
+        private void btnCalculateSequence_Click(object sender, EventArgs e)
+        {
+            int number = int.Parse(txtInput.Text);
+            int result = RecursiveFibonacci(number);
+            lblResult.Text = $"Result: {result}";
+        }
+
+    }
+}
