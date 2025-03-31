a25c53ee3ae8e3a36a8d6d7e531e0662baea7fa7
CpE Laboratory <CpE Laboratory@BE216-PC06>
2025-03-31 15:05:42 +0800

    Saving current project

--- Recursive Fibonacci Sequence/Form1.Designer.cs
+++ Recursive Fibonacci Sequence/Form1.Designer.cs
@@ -28,12 +28,55 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            button1 = new Button();
+            txtInput = new TextBox();
+            lblResult = new Label();
+            SuspendLayout();
+            // 
+            // button1
+            // 
+            button1.Location = new Point(53, 77);
+            button1.Name = "button1";
+            button1.Size = new Size(75, 23);
+            button1.TabIndex = 0;
+            button1.Text = "Calculate";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += btnCalculateSequence_Click;
+            // 
+            // txtInput
+            // 
+            txtInput.Location = new Point(168, 77);
+            txtInput.Name = "txtInput";
+            txtInput.Size = new Size(115, 23);
+            txtInput.TabIndex = 1;
+            // 
+            // lblResult
+            // 
+            lblResult.AutoSize = true;
+            lblResult.Location = new Point(130, 124);
+            lblResult.Name = "lblResult";
+            lblResult.Size = new Size(38, 15);
+            lblResult.TabIndex = 2;
+            lblResult.Text = "label1";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(364, 283);
+            Controls.Add(lblResult);
+            Controls.Add(txtInput);
+            Controls.Add(button1);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Button button1;
+        private TextBox txtInput;
+        private Label lblResult;
     }
 }
--- Recursive Fibonacci Sequence/Form1.cs
+++ Recursive Fibonacci Sequence/Form1.cs
@@ -6,5 +6,18 @@ namespace Recursive_Fibonacci_Sequence
         {
             InitializeComponent();
         }
+
+        public int RecursiveFibonacci(int n)
+        {
+            if (n <= 1) return n;
+            return RecursiveFibonacci(n - 1) + RecursiveFibonacci(n - 2) ;
+        }
+        private void btnCalculateSequence_Click(object sender, EventArgs e)
+        {
+            int number = int.Parse(txtInput.Text);
+            int result = RecursiveFibonacci(number);
+            lblResult.Text = $"Result: {result}";
+        }
+
     }
 }
